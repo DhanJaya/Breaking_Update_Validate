81c9f2b6e7eed8c9a4ce85ccb562a6904e8654fd
dhanjaya <dhanujaya2019@gmail.com>
2026-07-05 10:32:15 +0530

    Updated guava version and changes test

--- src/test/java/com/range/RangesTest.java
+++ src/test/java/com/range/RangesTest.java
@@ -9,7 +9,7 @@ public class RangesTest {
     @Test
     public void testRange(){
         Ranges ranges = new Ranges();
-        String expected = "[1‥10]";
+        String expected = "[1..10]";
         Assertions.assertEquals(expected, ranges.getRange( Range.closed(1, 10)));
     }
 }
